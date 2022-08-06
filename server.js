7a6e874fe9e630f851b5858340cdd45fb5b4d1f4
Wlla Talafha <wlla.issa396@gmail.com>
2022-08-06 17:42:35 +0300

    done

--- server.js
+++ server.js
@@ -3,49 +3,17 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const axios = require('axios')
 const server = express();
 const PORT = process.env.PORT;
-const weatherData = require('./data/weather.json');
+const main = require('./modules/main');
+const weather = require('./modules/weather');
+const movie = require('./modules/movie');
 
-
-
-server.listen(PORT,()=>console.log("Server is ready!"));
+server.listen(PORT, () => console.log("Server is ready!"));
 
 server.use(cors());
 
-
-server.get('/weather',(req,res)=> {
-
-    let inputCityName = req.query.searchQuery;
-    let inputLat = req.query.lat;
-    let inputLon = req.query.lon;
-
-        
-    if(!inputCityName) {
-        res.status(400).send('Please follow the documentation and fill the requierd feild as the following ?searchQuery=city name');
-    }
-
-    let result = weatherData.find(item=>inputCityName.toLowerCase() === item.city_name.toLowerCase());
-   
-    
-    try {
-        let filteredData = result.data.map(item=> new ForCast(item));
-        res.status(200).send(filteredData);
-    }catch (error) {
-        res.status(400).send('Data not found');
-    }
-
-});
-
-server.get('*',(req,res)=> {
-    res.status(500).send("Not found")
-})
-
-
-class ForCast {
-    constructor(day) {
-
-        this.date = day.valid_date;
-        this.description = day.weather.description ;
-    }
-}
+server.get('/weather', weather.handleWeather);
+server.get('/movie', movie.handleMovie);
+server.get('*', main.handleMain);
